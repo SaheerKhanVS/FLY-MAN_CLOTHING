f6ffa65350ed9587c2078bc0d66d5ad1cf4b6647
humanrobot <humanrobot1@humanrobot.local>
2026-06-23 18:22:19 +0530

    Fix Django settings module after project rename

--- flymenclothing/asgi.py
+++ flymenclothing/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flymanclothing.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flymenclothing.settings')
 
 application = get_asgi_application()
